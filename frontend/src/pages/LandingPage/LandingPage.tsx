f185748e8023e8520108ce1947418a09ef95ce49
Gabriel Padilla <g.padilla@uniandes.edu.co>
2025-04-03 15:17:14 +0800

    logo

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom";
+import NavBar from "../../components/NavBar";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      <h1 className="text-4xl font-bold mb-6">
-        Bienvenido a la Calculadora Exponencial
-      </h1>
-      <Link to="/calculadora" className="text-blue-500 hover:underline text-xl">
-        Ir a la Calculadora
-      </Link>
-    </div>
+    <>
+      <NavBar />
+      <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+        <h1 className="text-4xl font-bold mb-6">
+          Bienvenido a la Calculadora Exponencial
+        </h1>
+        <a
+          href="/calculadora"
+          className="text-blue-500 hover:underline text-xl"
+        >
+          Ir a la Calculadora
+        </a>
+      </div>
+    </>
   );
 };
 
