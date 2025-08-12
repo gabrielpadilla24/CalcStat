4f4add645560e4c546817883f6baac7ea2422bf9
Gabriel Padilla <g.padilla@uniandes.edu.co>
2025-08-12 09:22:41 -0500

    works but different cards

--- frontend/src/pages/CalculusCalculator/DifferentialCalculus/TangentLine/components/TangentLineInput.tsx
+++ frontend/src/pages/CalculusCalculator/DifferentialCalculus/TangentLine/components/TangentLineInput.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import MathFunctionInput from "@/components/MathFunctionInput";
-
+import CoordinatesInput, {
+  CoordinatesValue,
+} from "@/components/CoordenatesInput";
 type TangentLineResponse = {
   original: string;
   fxTangent: string;
@@ -16,16 +19,25 @@ const TangentLineInput = ({
     derivative: string,
     my: string
   ) => void;
-}) => (
-  <MathFunctionInput<TangentLineResponse>
-    label="Enter a function to find the tangent line:"
-    endpoint="http://localhost:8000/tangentline"
-    payloadKey="equation"
-    buttonText="Calculate Tangent Line"
-    onSuccess={(data) =>
-      onResult(data.original, data.fxTangent, data.derivative, data.my)
-    }
-  />
-);
+}) => {
+  const [coords, setCoords] = useState<CoordinatesValue>({});
+
+  return (
+    <div className="w-full">
+      <CoordinatesInput onChange={setCoords} />
+
+      <MathFunctionInput<TangentLineResponse, CoordinatesValue>
+        label="Enter a function to find the tangent line:"
+        endpoint="http://localhost:8000/tangentline"
+        payloadKey="equation"
+        extraPayload={coords}
+        buttonText="Calculate Tangent Line"
+        onSuccess={(data) =>
+          onResult(data.original, data.fxTangent, data.derivative, data.my)
+        }
+      />
+    </div>
+  );
+};
 
 export default TangentLineInput;
