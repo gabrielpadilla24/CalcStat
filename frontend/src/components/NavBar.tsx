432b1366780658feba053e23814eb50bec0cecc6
Gabriel Padilla <g.padilla@uniandes.edu.co>
2025-09-18 21:17:33 -0500

    navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -335,16 +335,6 @@ const NavBar = () => {
                 </div>
               )}
             </li>
-
-            {/* Contact */}
-            <li>
-              <Link
-                to="/contact"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 hover:text-[#5FBA9B] md:hover:bg-transparent md:border-0 md:p-0 dark:text-[#5FBA9B] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </Link>
-            </li>
           </ul>
         </div>
       </div>
